8b8353fb3ef04873f046038b66651d67caa3b347
Abdullah <aosama@useconsulting.co>
2025-03-28 07:12:59 +0200

    ibm: revision

--- src/tests/ibm/dec2024/MaxManagerMeetingScore.java
+++ src/tests/ibm/dec2024/MaxManagerMeetingScore.java
@@ -1,21 +1,50 @@
 package tests.ibm.dec2024;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MaxManagerMeetingScore {
 
-    public int getMax(List<Integer> meetingScore) {
-        meetingScore.sort(Integer::compareTo);
-        int counts = 0;
-        int score = 0;
+//    public int getMax(List<Integer> meetingScore) {
+//        meetingScore.sort(Integer::compareTo);
+//        int counts = 0;
+//        int score = 0;
+//
+//        for (int i = 1; i < meetingScore.size(); i++) {
+//            score += meetingScore.get(i);
+//            if (score > 0) {
+//                counts++;
+//            }
+//        }
+//
+//        return counts;
+//    }
 
-        for (int i = 1; i < meetingScore.size(); i++) {
-            score += meetingScore.get(i);
-            if (score > 0) {
-                counts++;
+    public int getMax(List<Integer> meetingScore) {
+        List<Integer> neg = new ArrayList<>();
+        int sum = 0;
+
+        for (Integer integer : meetingScore) {
+            if (integer < 0) {
+                neg.add(integer);
             }
+            sum += integer;
         }
 
-        return counts;
+        if (sum > 0) {
+            return meetingScore.size();
+        }
+        int counts = meetingScore.size();
+        neg.sort(Collections.reverseOrder());
+        for (int num : neg) {
+            counts--;
+            sum -= num;
+            if (sum > 0) {
+                return counts;
+            }
+        }
+        return -1;
     }
+
 }
--- src/tests/ibm/dec2024/gatways_and_firwals.sql
+++ src/tests/ibm/dec2024/gatways_and_firwals.sql
@@ -1,6 +1,72 @@
 -- u have gatways with ids and has firewal ref
 -- also u have firewals with no of rules
--- print gatways id along with tot ruls for firewall for gatways with more than 1 firewall
+-- print gatways id along with tot ruls for firewalls for gatways with more than 1 firewall
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+SELECT
+    gatway.id as id,
+    sum(no_of_rules) as tot_rules_no
+FROM
+    gatways gatway
+LEFT JOIN
+    firewals firewal
+ON
+    gatway.firewal_id = firewal.id
+GROUP BY
+    gatway.id
+HAVING
+    sum(no_of_rules) > 100 && count(gatway.firewal) > 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 SELECT
     g.gateway_id,
@@ -12,5 +78,5 @@ FROM
 GROUP BY
     g.gateway_id
 HAVING
-    COUNT(g.gateway_id) > 1
+    COUNT(g.gateway_id) > 1 --U can't use the total_rules in the select in the aggregation(aggregation happens before the select)
    AND SUM(f.rule_count) >= 100;
