9b16caf5d9731e8e362dd219e188e9dfc86d4467
Abdullah <aosama@useconsulting.co>
2025-05-03 16:14:44 +0300

    https://leetcode.com/problems/longest-word-in-dictionary/

---
+++ src/strings/LongestPalindromicSubstring.java
@@ -0,0 +1,69 @@
+package strings;
+
+public class LongestPalindromicSubstring {
+
+    public String longestPalindrome(String s) {
+        String longest = "";
+        String[][] dp = new String[s.length()][s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            String _1 = longestPalindromeRec(s, dp, i, i);
+            String _2 = longestPalindromeRec(s, dp, i, i + 1);
+            if (_1.length() > _2.length()) {
+                _2 = _1;
+            }
+            if (_2.length() > longest.length()) {
+                longest = _2;
+            }
+        }
+        return longest;
+    }
+
+    public String longestPalindromeRec(String s, String[][] dp, int i, int j) {
+        if (i < 0 || j == s.length()) return "";
+
+        if (dp[i][j] != null) {
+            return dp[i][j];
+        }
+        while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
+            i--;
+            j++;
+        }
+        if ((j == s.length() && i < 0) || i < 0 || j == s.length()) {
+            i++;
+            j--;
+        }
+        String palindrome = s.charAt(i) != s.charAt(j) ? s.substring(i + 1, j) : s.substring(i, j + 1);
+        dp[i][j] = palindrome;
+        return palindrome;
+    }
+
+//    simple intuitive
+    class Solution {
+        String longestSoFar = "";
+        public String longestPalindrome(String s) {
+            for (int i = 0 ; i < s.length(); i++) {
+                if (s.length() - i <= longestSoFar.length() / 2) break;
+                if (i + 1 < s.length() && s.charAt(i) == s.charAt(i + 1))  helper(s, i, i + 1);
+                helper(s, i, i);
+            }
+            return longestSoFar;
+        }
+
+        void helper(String s, int i, int j) {
+            do {
+                i--;
+                j++;
+            }
+            while(i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j));
+            i++;
+            j--;
+            if (j - i + 1 > longestSoFar.length()) {
+                longestSoFar = s.substring(i, j+1);
+            }
+        }
+    }
+    public static void main(String[] args) {
+    }
+
+}
---
+++ src/trie/Trie.java
@@ -0,0 +1,50 @@
+package trie;
+
+import java.util.*;
+
+public class Trie {
+    Character c;
+    Map<Character, Trie> children;
+    boolean valid;
+
+    public Trie() {
+        this.children = new HashMap<>();
+    }
+
+    public Trie(char c) {
+        this.c = c;
+        this.children = new HashMap<>();
+    }
+
+    public Trie findChild(char val) {
+        return children.get(val);
+    }
+
+    public boolean hasChild(char val) {
+        return children.containsKey(val);
+    }
+
+    public Trie addChild(char val) {
+        Trie child = new Trie(val);
+        child.valid = true;
+        this.children.put(val, child);
+        return child;
+    }
+
+    public boolean isLeaf() {
+        return children == null || children.isEmpty();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Trie trie = (Trie) o;
+        return Objects.equals(c, trie.c);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(c);
+    }
+}
