f8f64ce85f18f76f0854546258fed1c9ee2e2f42
Abdullah <aosama@useconsulting.co>
2025-05-16 05:16:13 +0300

    Revision

--- src/arrays/RemoveDuplicates.java
+++ src/arrays/RemoveDuplicates.java
@@ -1,6 +1,18 @@
 package arrays;
 
 public class RemoveDuplicates {
+
+    public int removeDuplicates_simple_better(int[] nums) {
+        int i = 0;  //ptr to the last unique elem
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
     public int removeDuplicates(int[] nums) {
         int idx, i = 0, j = 0;
         for (idx = 0; idx < nums.length; idx++) {
--- src/arrays/RemoveElement.java
+++ src/arrays/RemoveElement.java
@@ -4,7 +4,7 @@ public class RemoveElement {
     //Space O(1), time O(n)
     public int removeElement(int[] nums, int val) {
         int i = 0, j = 0;
-        while (j < nums.length) {
+        while (j < nums.length)  {
             if (nums[j] == val) {
                 j++;
                 continue;
@@ -14,6 +14,28 @@ public class RemoveElement {
             i++;
         }
         return i;
+    }
+
+    //More Intuitive Sol
+    public int removeElement_2(int[] nums, int val) {
+        int n = nums.length;
+        int r = n - 1;
+        int counts = 0;
+
+        for (int l = 0; l < n; l++) {
+            if (nums[l] == val) {
+                counts++;
+                while (r >= 0 && nums[r] == val) {
+                    r--;
+                }
+                if (r >= 0) {
+                    nums[l] = nums[r];
+                    r--;
+                }
+            }
+        }
+
+        return n - counts;
     }
 
 }
