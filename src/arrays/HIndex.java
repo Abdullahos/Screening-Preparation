c2c23193c4f1895dbb31956eb25d12dab3fe74c7
Abdullah <aosama@useconsulting.co>
2025-05-19 17:35:37 +0300

    https://leetcode.com/problems/h-index/submissions/1638343991/?envType=study-plan-v2&envId=top-interview-150

--- src/arrays/HIndex.java
+++ src/arrays/HIndex.java
@@ -1,21 +1,17 @@
 package arrays;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 /*
     [3,0,6,1,5]
-
     [0,1,3,5,6], ans: r
 
 
     [1,3,1]
-
     [1,1,3], ans [1]
 
 
     [100,200,1]
-
     [1,100,200] ans 2
  */
 public class HIndex {
