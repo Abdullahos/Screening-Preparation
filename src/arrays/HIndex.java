b5418509362525919d6f789f34d47208f74d25f0
Abdullah <aosama@useconsulting.co>
2025-05-19 17:34:37 +0300

    https://leetcode.com/problems/h-index/submissions/1638343991/?envType=study-plan-v2&envId=top-interview-150

---
+++ src/arrays/HIndex.java
@@ -0,0 +1,37 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/*
+    [3,0,6,1,5]
+
+    [0,1,3,5,6], ans: r
+
+
+    [1,3,1]
+
+    [1,1,3], ans [1]
+
+
+    [100,200,1]
+
+    [1,100,200]
+ */
+public class HIndex {
+
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        if (n == 0) return 0;
+
+        Arrays.sort(citations);
+        int i = n;
+
+        while (i - 1 >= 0 && citations[i - 1] >=  n - i + 1) {
+            i--;
+        }
+
+        return n - i;
+    }
+
+}
