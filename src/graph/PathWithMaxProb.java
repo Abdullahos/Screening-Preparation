bc7983458bd18e5ff677f8651956e5a31a811d8e
Abdullah <abdullahelagami55@gmail.com>
2025-08-03 18:19:43 +0300

    Improve performance by using Class Node to avoid  casting overhead
    https://leetcode.com/problems/path-with-maximum-probability

--- src/graph/PathWithMaxProb.java
+++ src/graph/PathWithMaxProb.java
@@ -4,51 +4,61 @@ import java.util.*;
 
 public class PathWithMaxProb {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
-        Map<Integer, List<double[]>> graph = new HashMap<>();
+        List<List<Node>> graph = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
 
         for (int i = 0; i < edges.length; i++) {
             int[] edge = edges[i];
-            graph.computeIfAbsent(edge[0], a -> new ArrayList<>()).add(new double[]{edge[1], succProb[i]});
-            graph.computeIfAbsent(edge[1], a -> new ArrayList<>()).add(new double[]{edge[0], succProb[i]});
+            graph.get(edge[0]).add(new Node(edge[1], succProb[i]));
+            graph.get(edge[1]).add(new Node(edge[0], succProb[i]));
         }
 
-        Queue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[2], a[2]));
+        Queue<Node> pq = new PriorityQueue<>((a, b) -> Double.compare(b.prob, a.prob));
 
-        pq.offer(new double[]{start_node, start_node, 1.0});    //from, to, prob
+        pq.offer(new Node(start_node, 1.0));    //to, prob
 
-        double[] visited = new double[n];
-        Arrays.fill(visited, -1);
+        double[] maxProb = new double[n];
+        Arrays.fill(maxProb, -1);
 
         while (!pq.isEmpty()) {
-            double[] e = pq.remove();
+            Node node = pq.remove();
 
-            int u = (int) e[0];
-            int v = (int) e[1];
-            double w = e[2];
+            int u = node.u;
+            double w = node.prob;
 
-            if (v == end_node) {
+            if (u == end_node) {
                 return w;
             }
 
-            if (visited[v] >= w) {
+            if (maxProb[u] >= w) {
                 continue;
             }
 
-            visited[v] = w;
+            maxProb[u] = w;
 
-            if (!graph.containsKey(v)) {
-                continue;
-            }
-
-            for (double[] mapping : graph.get(v)) {
-                if (mapping[0] != u) {
-                    pq.offer(new double[]{v, mapping[0], w * mapping[1]});
+            for (Node mapping : graph.get(u)) {
+                double accProb = w * mapping.prob;
+                if (maxProb[mapping.u] < accProb) {
+                    pq.offer(new Node(mapping.u, accProb));
                 }
             }
 
         }
 
         return 0;
+    }
+
+    class Node {
+        int u;
+        double prob;
+
+        public Node(int u, double prob) {
+            this.u = u;
+            this.prob = prob;
+        }
     }
 
     public static void main(String[] args) {
@@ -60,7 +70,7 @@ public class PathWithMaxProb {
                 {0, 2}
         };
 
-        double[] successProb = new double[]{0.5,0.5,0.2};
+        double[] successProb = new double[]{0.5, 0.5, 0.2};
 
         path.maxProbability(3, arr, successProb, 0, 2);
 
