f638b651a5fe1252af27fe8cf132d915dfb62f32
Abdullah <abdullahelagami55@gmail.com>
2025-10-12 15:22:40 +0300

    Resolve https://leetcode.com/problems/h-index/?envType=study-plan-v2&envId=top-interview-150

--- src/arrays/HIndex.java
+++ src/arrays/HIndex.java
@@ -15,6 +15,7 @@ import java.util.Arrays;
     [1,100,200] ans 2
  */
 public class HIndex {
+    int maxCitationsPerPapar = 1000;
 
     public int hIndex(int[] citations) {
         int n = citations.length;
@@ -31,6 +32,7 @@ public class HIndex {
     }
 
     //Using count sort, Bucket sort
+    //optimized version of v3
     public int hIndex2(int[] citations) {
         int n = citations.length;
         if (n == 0)
@@ -50,6 +52,46 @@ public class HIndex {
             total += counts[i];
             if (total >= i) {
                 return i;   //early return for the max i- > max H
+            }
+        }
+
+        return 0;
+    }
+
+    public int hIndex3(int[] citations) {
+
+        int n = citations.length;
+        int[] arr = new int[maxCitationsPerPapar + 1];
+
+        for (int cit : citations) {
+            for (int i = cit; i >= 0; i--) {
+                arr[i]++;
+            }
+        }
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] >= i) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
+    public int optimizedHIndex3(int[] citations) {
+
+        int n = citations.length;
+        int[] arr = new int[maxCitationsPerPapar + 1];
+
+        for (int cit : citations) {
+            arr[cit]++;
+        }
+
+        int total = 0;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            total += arr[i];
+            if (total >= i) {
+                return i;
             }
         }
 
