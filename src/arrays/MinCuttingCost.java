29f7f816c4c49d7d71652c80f49291c62f3aa4a3
Abdullah <abdullahelagami55@gmail.com>
2025-10-12 13:50:33 +0300

    simpler solution for better time complexity

--- src/arrays/MinCuttingCost.java
+++ src/arrays/MinCuttingCost.java
@@ -3,6 +3,16 @@ package arrays;
 public class MinCuttingCost {
 
     public long minCuttingCost(int n, int m, int k) {
+        if (Math.max(n, m) <= k) {
+            return 0;
+        }
+
+        return ((long) (Math.max(n, m) - k)) * k;
+    }
+
+    //no need to do so, as no container can hold more than one log, and it's granted to fit as
+    //the problem stated, so the min one will absolutely fit and the max maybe, so we can rely on this formula ((long) (Math.max(n, m) - k)) * k
+    public long minCuttingCost2(int n, int m, int k) {
         int max = Math.max(n, m);
 
         if (k >= max) {
