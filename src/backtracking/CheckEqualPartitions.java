32cf1b24c73e1121ae2171e0667d70cbb5637a0c
Abdullah <abdullahelagami55@gmail.com>
2025-10-27 20:32:24 +0300

    leetcode: backtrack & dp : Optimize CheckEqualPartitions

--- src/backtracking/CheckEqualPartitions.java
+++ src/backtracking/CheckEqualPartitions.java
@@ -1,7 +1,6 @@
 package backtracking;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 3,1,6,8,4
@@ -17,37 +16,73 @@ import java.util.Set;
 public class CheckEqualPartitions {
 
     public boolean checkEqualPartitions(int[] nums, long target) {
-        return dfs(nums, target, 1, new HashSet<>(), 0);
-    }
+        long mult = 1;
 
-    private boolean dfs(int[] nums, long target, long acc, Set<Integer> inSub, int start) {
-        if (acc > target) {
+        for (int num : nums) {
+            mult *= num;
+        }
+
+        if (mult < target) {
             return false;
         }
 
-        if (acc == target) {
-            acc = 1;
-            for (int num : nums) {
-                if (acc > target) {
-                    return false;
-                }
-                if (!inSub.contains(num)) {
-                    acc *= num;
-                }
-            }
-            return acc == target;
+        Set<CompositeKey> dp = new HashSet<>();
+
+        return backtrack(nums, target, 1, mult, 0, dp);
+    }
+
+    private boolean backtrack(int[] nums, long target, long multAcc, long rest, int start, Set<CompositeKey> dp) {
+        if (multAcc == rest && multAcc== target) {
+            return true;
         }
 
-        for (int i = start; i < nums.length; i++) {
-            if (!inSub.contains(nums[i])) {
-                inSub.add(nums[i]);
-                if (dfs(nums, target, acc * nums[i], inSub, i + 1)) {
-                    return true;
-                }
-                inSub.remove(nums[i]);
-            }
+        if (start == nums.length) {
+            return false;
+        }
+        CompositeKey key = getKey(multAcc, start);
+
+        if (dp.contains(key)) {
+            return false;
         }
 
-        return false;
+        if (multAcc > target) {
+            return false;
+        }
+
+        boolean found = backtrack(nums, target, multAcc * nums[start], rest / nums[start], start + 1, dp) || backtrack(nums, target, multAcc, rest, start + 1, dp);
+
+        if (!found) {
+            dp.add(key);
+        }
+
+        return found;
+    }
+
+    private CompositeKey getKey(long multAcc, int start) {
+        return new CompositeKey(start, multAcc);
+    }
+
+    class CompositeKey {
+        private final Integer index;
+        private final Long product;
+
+        public CompositeKey(Integer index, Long product) {
+            this.index = index;
+            this.product = product;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            CompositeKey that = (CompositeKey) o;
+            return Objects.equals(index, that.index) &&
+                    Objects.equals(product, that.product);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(index, product);
+        }
     }
 }
