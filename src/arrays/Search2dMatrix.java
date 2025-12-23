88ef3b3a503d9f7cc3d965d1001abc8e6238fd03
Abdullah <abdullahelagami55@gmail.com>
2025-12-23 17:05:58 +0200

    LeetCode: Search2dMatrix t: (n log n), s: O(1) solution

--- src/arrays/Search2dMatrix.java
+++ src/arrays/Search2dMatrix.java
@@ -1,8 +1,30 @@
 package arrays;
 
+import java.util.Arrays;
+
 // https://leetcode.com/problems/search-a-2d-matrix-ii/?envType=problem-list-v2&envId=55vr69d7
 public class Search2dMatrix {
+
+  //t: O(n log n), s: O(1)
   public boolean searchMatrix(int[][] matrix, int target) {
+    int n = matrix.length, m = matrix[0].length;
+    int r = 0;
+
+    while (r < n && matrix[r][0] <= target) {
+      int[] row = matrix[r];
+
+      int idx = Arrays.binarySearch(row, target);
+
+      if (idx >= 0) return true;
+
+      r++;
+    }
+
+    return false;
+  }
+
+  //t: O(n * m), s: O(n * m)
+  public boolean searchMatrix_n_m(int[][] matrix, int target) {
     int n = matrix.length, m = matrix[0].length;
     boolean[][] visited = new boolean[n][m];
     return search(matrix, target, n, m, 0, 0, visited);
